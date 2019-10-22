9ee07533631dfd36e9066780d6afa89a706badcc
Florian <florian@bildspur.ch>
2019-10-22 18:19:20 +0200

    implemented average pixel detection

--- src/main/kotlin/ch/zhdk/tracking/pipeline/RGBIRPipeline.kt
+++ src/main/kotlin/ch/zhdk/tracking/pipeline/RGBIRPipeline.kt
@@ -2,9 +2,7 @@ package ch.zhdk.tracking.pipeline
 
 import ch.zhdk.tracking.config.PipelineConfig
 import ch.zhdk.tracking.io.InputProvider
-import ch.zhdk.tracking.javacv.convertColor
-import ch.zhdk.tracking.javacv.toPoint2d
-import ch.zhdk.tracking.javacv.transform
+import ch.zhdk.tracking.javacv.*
 import ch.zhdk.tracking.model.ActiveRegion
 import ch.zhdk.tracking.model.TactileObject
 import org.bytedeco.opencv.opencv_core.Mat
@@ -13,6 +11,7 @@ import org.bytedeco.javacpp.IntPointer
 import org.bytedeco.opencv.global.opencv_core.*
 import org.bytedeco.opencv.global.opencv_imgproc.*
 import org.bytedeco.opencv.opencv_core.Point
+import org.bytedeco.opencv.opencv_core.Point2d
 import org.bytedeco.opencv.opencv_core.Size
 import kotlin.math.roundToInt
 
@@ -34,35 +33,57 @@ class RGBIRPipeline(config: PipelineConfig, inputProvider: InputProvider, pipeli
             // extract color
             inRange(hsvMat, lowerColor, higherColor, mask)
 
-            //rangeMat.convertColor(COLOR_HSV2BGR)
-            //rangeMat.convertColor(COLOR_BGR2GRAY)
-
-            // extract brightest point
-            val minVal = DoublePointer()
-            val maxVal = DoublePointer()
-            val min = Point()
-            val max = Point()
-            minMaxLoc(mask, minVal, maxVal, min, max, null)
-
             // outline
             val result = Mat()
             bitwise_and(frame, frame, result, mask)
 
-
-
             // which one to display
             if(config.displayRangeOne.value && index == 1) {
-                result.copyTo(frame)
+                result.clone().copyTo(frame)
             }
 
             if(!config.displayRangeOne.value && index == 0) {
-                result.copyTo(frame)
+                result.clone().copyTo(frame)
+            }
+
+            // extract brightest point
+            result.convertColor(COLOR_BGR2GRAY)
+
+            // filter
+            if(config.morphologyFilterEnabled.value) {
+                frame.erode(config.erodeSize.value)
+                frame.dilate(config.dilateSize.value)
+            }
+
+            /*
+            // minmaxloc
+            val minVal = DoublePointer()
+            val maxVal = DoublePointer()
+            val min = Point()
+            val max = Point()
+            minMaxLoc(result, minVal, maxVal, min, max, null)
+             */
+
+            // detection through average
+            val nonZeroPixels = Mat()
+            findNonZero(result, nonZeroPixels)
+            val mm = moments(nonZeroPixels, false )
+            val moment10 = mm.m10()
+            val moment01 = mm.m01()
+            val moment00 = mm.m00()
+            val point = Point2d(moment10 / moment00, moment01 / moment00)
+
+            // fix nan bug
+            if(point.x().isNaN() || point.y().isNaN()) {
+                point.x(0.0)
+                point.y(0.0)
             }
 
             result.release()
 
-            ActiveRegion(max.toPoint2d().transform(5.0, 5.0),
-                max,
+            // create active region
+            ActiveRegion(point,
+                point.toPoint(),
                 Size(10, 10),
                 index.toDouble(),
                 timestamp)
