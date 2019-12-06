9149d84025bb3ed64805c59fd4017afb7f65839f
Luke Franzke <lukefranzke@gmail.com>
2019-12-06 10:24:44 +0100

    updated touch p5js demo

--- examples/Experiments/DemoMultitouchP5js/demo/demo.js
+++ examples/Experiments/DemoMultitouchP5js/demo/demo.js
@@ -1,23 +1,97 @@
+
+let maxDistance = 200;
+// Object tracking library:
+//https://github.com/andypotato/tritra
+// triangle with 18, 36 and 54 degrees vertex angles
+var vertexAngles = [18, 36, 54];
+
+// create the recognizer
+var R = new tritra.Recognizer(vertexAngles, {
+        /*
+          Apex angles have a distance of 18 degrees each therefore we choose
+          18 / 2 = 9 degrees of tolerance to each side. You can go lower than
+          that if your application doesn't require markers to be moved quickly.
+        */
+        maxAngleTolerance: 4,
+        /*
+          The maximum distance for two points to still be considered as part of
+          the same triangle. You should adjust these according to your screen
+          size and DPI. As a general rule, the larger the screen and the higher
+          the DPI, the higher you should set this value.
+          Do not set it too high though as this will cause two nearby markers to
+          no longer be recognized.
+        */
+        maxPointDistance: 170
+      });
+
+let mousePoints = [];;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   fill(255);
+  angleMode(DEGREES)
 }
+
+
 
 function draw() {
+  let points = [];
   background(255, 0, 0);
-  let display = touches.length + ' touches';
-
+  let display = 'touches:' + touches.length ;
+  let display2 =  'frame Rate: ' + floor(frameRate());
   text(display, 5, 10);
-  for (let i = 0; i < touches.length; i++) {
+  text(display2, 5, 20);
+  circle(mouseX,  mouseY, 15);
 
-    circle( touches[i].x,  touches[i].y, 50);
+  for (let i = 0; i < touches.length; i++) {
+    circle(touches[i].x,  touches[i].y, 10);
+    points.push(new tritra.Vector2d(touches[i].x, touches[i].y));
   }
-   circle(mouseX,  mouseY, 50);
+
+  for (let i = 0; i < mousePoints.length; i++) {
+    circle(mousePoints[i].x,  mousePoints[i].y, 10);
+    points.push(mousePoints[i]);
+  }
+
+
+
+  var matches = R.findMatches(points);
+  for (let i = 0; i < matches.length; i++) {
+      var centerPoint = matches[i].getCenter();
+      var orientation =  -matches[i].getOrientation();
+      // index within your vertexAngles
+      console.log(matches[i].getOrientation())
+      fill(0);
+      circle(centerPoint.x,  centerPoint.y, 200);
+      stroke(255);
+      strokeWeight(10);
+      line(centerPoint.x, centerPoint.y, centerPoint.x+(sin(orientation)*150), centerPoint.y+(cos(orientation)*150));
+      noStroke();
+      strokeWeight(1);
+  }
+
+// connecting lines
+  for (let i = 0; i < points.length; i++) {
+    circle(points[i].x,  points[i].y, 10);
+    for (let j = 0; j < points.length; j++) {
+      let distance = dist(points[i].x,  points[i].y, points[j].x,  points[j].y)
+      if (j!=i && distance<maxDistance) {
+       stroke(255);
+       line(points[i].x,  points[i].y, points[j].x,  points[j].y);
+       noStroke();
+      }
+    }
+  }
+
 }
+
+
+
 
 function mousePressed() {
     let fs = fullscreen();
+    mousePoints.push(new tritra.Vector2d(mouseX, mouseY));
     fullscreen();
 }
 
--- examples/Experiments/P5JSEarth/sketch.js
+++ examples/Experiments/P5JSEarth/sketch.js
@@ -1,9 +1,9 @@
-var img;
-var sky;
-var theta = 0.001;
-var r = 400;
-var easycam;
-
+let img;
+let sky;
+let theta = 0.001;
+let r = 400;
+let easycam;
+let toggle = false;
 function preload() {
   img = loadImage('earth.jpg');
 	sky = loadImage('sky.jpg');
@@ -11,7 +11,6 @@ function preload() {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
-	//setupOsc(12000, 8002);
 	noStroke();
 	easycam = createEasyCam();
 	// fix for EasyCam to work with p5 v0.7.2
@@ -69,7 +68,12 @@ function draw() {
 function draw2d() {
 	easycam.beginHUD()
 	ambientLight(255, 255, 255);
-	fill(78	,196,164);
+	if (toggle) {
+		fill(78	,196,164);
+	} else {
+		fill(255,0,0);
+	}
+
 	rect(100,100,100,100)
 	//pointLight(0, 0, 255, 0, 0, -600);
 //	pointLight(0, 0, 255, 0, 0, -600);
@@ -80,14 +84,8 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
-function receiveOsc(address, value) {
-	console.log("received OSC: " + address + ", " + value);
-
-	if (address == '/test') {
-		x = value[0];
-		y = value[1];
-	}
+function mouseClicked() {
+	toggle = !toggle;
 }
 
 function drawLine(x1, y1, z1, x2, y2, z2){
@@ -97,28 +95,4 @@ function drawLine(x1, y1, z1, x2, y2, z2){
   vertex(x2,y2,z2);
   endShape();
 	noStroke();
-}
-
-function sendOsc(address, value) {
-	socket.emit('message', [address].concat(value));
-}
-
-
-function setupOsc(oscPortIn, oscPortOut) {
-	var socket = io.connect('http://127.0.0.1:8081', { port: 8081, rememberTransport: false });
-	socket.on('connect', function() {
-		socket.emit('config', {
-			server: { port: oscPortIn,  host: '127.0.0.1'},
-			client: { port: oscPortOut, host: '127.0.0.1'}
-		});
-	});
-	socket.on('message', function(msg) {
-		if (msg[0] == '#bundle') {
-			for (var i=2; i<msg.length; i++) {
-				receiveOsc(msg[i][0], msg[i].splice(1));
-			}
-		} else {
-			receiveOsc(msg[0], msg.splice(1));
-		}
-	});
 }
