c86440aec285f98c8087510b0d5f88eafe6bed5c
Florian Bruggisser <florian@bildspur.ch>
2020-07-22 14:24:41 +0200

    added basic logging library

--- hardware/TactileDevice/src/main.cpp
+++ hardware/TactileDevice/src/main.cpp
@@ -1,9 +1,21 @@
+#include <HardwareSerial.h>
 #include "Arduino.h"
 
-void setup() {
+// clion ugly import
+#include "../.pio/libdeps/esp32dev/log4arduino/src/log4arduino.h"
 
+#define ENABLE_LOG4ARDUINO
+
+void setup() {
+#ifdef ENABLE_LOG4ARDUINO
+    // setup serial
+    Serial.begin(115200);
+#endif
+    LOG_INIT(&Serial);
+    LOG("hello world!");
 }
 
 void loop() {
-
+    delay(1000);
+    LOG("test");
 }
