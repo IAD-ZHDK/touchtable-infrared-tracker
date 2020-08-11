6f939a9662311bbfe1e4892041ad13680d3e1ce9
Florian Bruggisser <florian@bildspur.ch>
2020-08-11 18:17:35 +0200

    switched scan response to true

--- hardware/Beacon_code/ESP32/src/main.cpp
+++ hardware/Beacon_code/ESP32/src/main.cpp
@@ -164,7 +164,7 @@ void setup() {
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
+  pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
